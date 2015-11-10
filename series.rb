8edf6b60b5ecf602184d57016486f4bc06ce3f7c
Chip Self <chipself9@gmail.com>
2015-11-10 17:10:50 -0500

    added series solution

---
+++ series.rb
@@ -0,0 +1,22 @@
+class Series
+
+
+  def initialize(slice_string)
+    @slice_string = slice_string
+  end
+
+
+  def slices(length)
+    raise ArgumentError.new if length > @slice_string.length
+    outer_array = []
+    index = 0
+      until index == @slice_string.length do
+        string = @slice_string[index, length]
+        fixnum = string.chars.map(&:to_i)
+        outer_array << fixnum if fixnum.length == length
+        index += 1
+      end
+    outer_array
+  end
+
+end
